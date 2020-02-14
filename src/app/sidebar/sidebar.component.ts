a3dd9b4f46fbe085d0702b8facaa8a0c74d550d4
Nadezhda Chueva <Nadezhda@linux.fritz.box>
2020-02-15 00:00:06 +0100

    ng hooks

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, ViewChild, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, ViewChild, Output, OnChanges, SimpleChanges, AfterContentChecked, AfterContentInit, AfterViewInit, AfterViewChecked, DoCheck } from '@angular/core';
 import { MatDrawer } from '@angular/material';
 
 @Component({
@@ -6,16 +6,47 @@ import { MatDrawer } from '@angular/material';
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.sass']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterViewInit, AfterContentChecked, AfterViewChecked {
 
   @Output()
   public setSidenavControl: EventEmitter<MatDrawer> = new EventEmitter<MatDrawer>(true);
 
-  @ViewChild('drawer', {static: true})
+  @ViewChild('drawer', { static: true })
   public drawer!: MatDrawer;
+
+  constructor() {
+    console.log('constructor');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+  }
 
   ngOnInit() {
     this.setSidenavControl.emit(this.drawer);
+    console.log('ngOnInit');
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+
+  };
+
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('AfterViewInit');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
   }
 
 }
