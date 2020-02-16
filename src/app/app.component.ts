f925d9b889d09fc96faeacc56ae6b101cd51d8fe
Nadezhda Chueva <Nadezhda@8f908a3c-85b3-45cc-a544-4af66513aaf1.fritz.box>
2020-02-16 11:52:19 +0100

    initial pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
 import { MatDrawer } from '@angular/material';
+import { products$, IProduct } from './mock';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
+  encapsulation: ViewEncapsulation.Emulated,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  public constructor() { }
   public title = 'ngPracticeCourse';
   public drawerSidenav: MatDrawer;
+  public products: IProduct[] = [];
+
+  public account = {
+    name: 'Ihor',
+    age: 33,
+  };
+
+
+  public products$: Observable<IProduct[]> = products$;
 
   public setSidenav(drawer: MatDrawer) {
     this.drawerSidenav = drawer;
   }
+  public ngOnInit(): void {
+    this.products$.subscribe( (products) => {
+      console.log(products);
 
-  public constructor() {}
+      this.products = products;
+    } );
+  }
+
 }
